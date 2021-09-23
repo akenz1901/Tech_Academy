8587d484d382d5d93d8079f0b21200bd7eab2fef
akenz1901 <michael_aka1@icloud.com>
2021-09-23 15:18:29 +0100

    Added models

--- tech_academy/models.py
+++ tech_academy/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Cohort(models.Model):
@@ -7,15 +8,24 @@ class Cohort(models.Model):
     date_created = models.DateTimeField("Date created")
 
     def __str__(self):
-        return self.name, self.description
+        return self.name
 
 
 class Native(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
-    image = models.ImageField(default="", upload_to="templates/native_images/")
-    cohort = models.OneToOneField(Cohort, on_delete=models.DO_NOTHING)
-    date_added = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="native_images/")
+    cohort = models.ForeignKey(Cohort, on_delete=models.DO_NOTHING)
+    date_added = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+
+class Thought(models.Model):
+    thought_text = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now())
+    native = models.OneToOneField(Native, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.thought_text[:50]
