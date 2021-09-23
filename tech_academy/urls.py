726e2fc055ef90e2ece56b140628aff00982ed62
akenz1901 <michael_aka1@icloud.com>
2021-09-23 15:25:10 +0100

    Urls patterns are perfectly structured to take request

--- tech_academy/urls.py
+++ tech_academy/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('create/cohort/', views.create_cohort, name='create_cohort')
+    path('cohort/create/', views.create_cohort, name='create_cohort'),
+    path('cohort/<int:pk>/', views.cohort_detail, name='cohort_detail'),
+    path('native/list/', views.native_list, name='native_list'),
+    path('native/add/', views.add_native, name='add_native'),
+    path('native/<int:pk>/detail/', views.native_detail, name='native_detail'),
 ]
