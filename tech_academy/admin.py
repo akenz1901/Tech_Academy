193076bcd405c5e00ea4002215561b8a3d7400ad
akenz1901 <michael_aka1@icloud.com>
2021-09-23 15:09:36 +0100

    registered all model to admin site panel

--- tech_academy/admin.py
+++ tech_academy/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Cohort, Native
+from .models import Cohort, Native, Thought
 
 admin.site.register(Cohort)
 admin.site.register(Native)
+admin.site.register(Thought)
+
